703bd5a352e5d54bde543005156fc4686b3200a4
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-03-27 18:03:14 +0000

    Initial commit

---
+++ live/django_intro/django_intro/urls.py
@@ -0,0 +1,32 @@
+"""django_intro URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pages import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('hello/', views.hello),
+    path('lotto/', views.lotto),
+    path('cube/<int:num>/', views.cube),
+    path('about_me/',views.about_me),
+    path('lunch/', views.lunch),
+    #variable routing
+    path('hi/<str:name>/', views.hi),
+    path('add/<int:a>/<int:b>', views.add),
+    path('dinner/<str:menu>/<int:num>/', views.dinner)
+]
---
+++ live/django_intro/pages/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+
+import random
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request):
+    return render(request, 'hello.html')
+
+def lotto(request):
+
+    pick = sorted(random.sample(range(1,46),6))
+    context = {'pick':pick,}
+    # return render(request, 'lotto.html', context)
+    return render(request, 'lotto.html', {'pick':pick})
+
+def cube(request, num):
+    return render(request, 'cube.html',{'result':num**3})
+
+def about_me(request):
+    return render(request, 'about_me.html')
+
+def lunch(request):
+    options = ['Waffle', 'Pizza', 'Fried chicken', 'Sushi', 'Fork chop', 'Grenola & Yogurt']
+    menu = random.choice(options)
+    context = {
+        'menu': menu,
+        'options': options
+    }
+
+    return render(request, 'lunch.html', context)
+
+def hi(request, name):
+    context = {
+        'name': name
+    }
+    return render(request, 'hi.html',context)
+
+def add(request, a, b):
+    result = a+ b
+    context = {
+        'a': a,
+        'b':b,
+        'result': result
+    }
+    return render(request,'add.html', context)
+
+
+def dinner(reqeust, menu, num):
+    context = {
+        'menu': menu,
+        'num' : num,
+    }
+    return render(reqeust, 'dinner.html', context)
