165e8ae0ac6d40bf93714ee60f3434df7300d430
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-04-28 14:42:54 +0900

    Add User model w/ custom User

--- live/instagram/accounts/models.py
+++ live/instagram/accounts/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
 # model은 필요없다! Django package에 있는 User를 사용할 것이기 때문!
+
+# 사용자 정의 모델 만들기
+class User(AbstractUser):
+    followers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name = 'followings'
+    )
--- live/instagram/instagram/settings.py
+++ live/instagram/instagram/settings.py
@@ -130,3 +130,6 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+# 내가 만든 User model 사용한다고 선언
+AUTH_USER_MODEL = 'accounts.User'
